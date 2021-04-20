071c40ddf2f267fe479c4049caf1656d293fe9bc
rni <rni@xpanxion.com>
2021-04-20 10:40:47 -0400

    Created base structure of Angular Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './components/employee/employee.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { ProductComponent } from './components/product/product.component';
+import { OrderComponent } from './components/order/order.component';
+import { ReportsComponent } from './components/reports/reports.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    CustomerComponent,
+    ProductComponent,
+    OrderComponent,
+    ReportsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
